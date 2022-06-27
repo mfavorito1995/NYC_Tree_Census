7b91c4de96858c2da707e7f4ae902743fa8ebefb
mfavorito1995 <34494346+mfavorito1995@users.noreply.github.com>
2022-06-27 08:46:42 -0400

    Add files via upload

---
+++ Overview.py
@@ -0,0 +1,161 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import streamlit_folium
+import geopandas as gpd
+import json
+
+
+st.set_page_config(
+    page_title="NYC Tree Census",
+    page_icon=':deciduous_tree:',
+    layout="wide"
+)
+
+st.title('A Look at the NYC Tree Census of 2015')
+
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+# Notify the reader that the data was successfully loaded.
+data_load_state.text('Loading data...done!')
+
+st.markdown('### Introduction')
+
+st.markdown("In 2015 the city of New York conducted its third Street Tree Census. "
+            "City employees and volunteers *(also known as voluntreers)* went block by block to count all of the leafy friends lining the streets of the five boros. "
+            "According to the Parks Department's website, this effort brought together **more than 2,200 volunteers** who counted a total of **666,134 trees** using a combination of old school tools and high tech interactive maps. "
+            "Importantly, as its name makes clear, this census only counted trees lining the streets and *not* those found in the city's many parks.")
+
+st.markdown(
+    "While a tree census may sound like just a fun bureaucratic exercise, understanding the landscape of city streets can have a real impact on New Yorkers' daily lives. "
+    "Street trees benefit residents in many ways such as improving air quality, reducing stormwater runoff, and lessening the urban heat island effect - all in addition to making streets more beautiful. "
+    "All told, the city has estimated that in addition to looking beautiful, street trees have created $151.2 million in benefits to New York City. ")
+
+st.markdown(
+    "Understanding these benefits, the city aims to use the data collected in their tree census to help them make better public policy decisions. "
+    "Since conducting the first Street Tree Census in 1995, the city has been better equipped to direct funding for greening intiatives, ultimately improving the lives of all New Yorkers.")
+
+st.markdown("Now, as a geospatial nerd, this census provided me with tons of fun new data to play with. "
+            "I've created this page to help do just that. **Scroll on to learn more!**")
+
+
+@st.cache
+def load_trees_data():
+    usecols = ['created_at', 'tree_id', 'block_id', 'the_geom', 'tree_dbh',
+               'stump_diam', 'curb_loc', 'status', 'health', 'spc_latin', 'spc_common',
+               'address', 'zipcode', 'cb_num', 'boroname', 'nta', 'nta_name',
+               'boro_ct', 'Latitude', 'longitude']
+
+    data = pd.read_csv(r'C:\Users\Mark\NYC Tree Data\2015StreetTreesCensus_TREES.csv', usecols=usecols)
+    data.rename({'Latitude': 'latitude'}, inplace=True)
+    data['spc_common'] = data['spc_common'].str.title()
+    return data
+
+
+def boroughs_map():
+    boroughs_gpd = gpd.read_file(r'C:\Users\Mark\NYC Tree Data\boroughs_with_stats.geojson').set_index("Boro")
+
+    m = px.choropleth_mapbox(boroughs_gpd, geojson=boroughs_gpd.geometry, locations=boroughs_gpd.index,
+                             color=boroughs_gpd['Count of Trees'],
+                             color_continuous_scale="YlGn",
+                             mapbox_style="carto-positron",
+                             zoom=9, center={"lat": 40.71264206172198648, "lon": -73.95069249193706},
+                             opacity=0.5,
+                             labels={'Count of Trees': 'Count of Trees'},
+                             height=700
+                             )
+    return m
+
+
+def nta_map():
+    nta_gpd = gpd.read_file(r'C:\Users\Mark\NYC Tree Data\ntas_with_stats.geojson').set_index("NTA")
+
+    m = px.choropleth_mapbox(nta_gpd, geojson=nta_gpd.geometry,
+                             locations=nta_gpd.index,
+                             color=nta_gpd['Count of Trees'],
+                             color_continuous_scale="YlGn",
+                             mapbox_style="carto-positron",
+                             zoom=9, center={"lat": 40.71264206172198648, "lon": -73.95069249193706},
+                             opacity=0.5,
+                             labels={'NTA': 'NTA', 'Count of Trees': 'Count of Trees'},
+                             height=700
+                             )
+
+    return m
+
+
+def hex_map():
+    hex_gpd = gpd.read_file(r'C:\Users\Mark\NYC Tree Data\quarter_mile_trees.geojson')
+
+    m = px.choropleth_mapbox(hex_gpd, geojson=hex_gpd.geometry,
+                             locations=hex_gpd.index,
+                             color=hex_gpd['Count Trees'],
+                             color_continuous_scale="YlGn",
+                             mapbox_style="carto-positron",
+                             zoom=9, center={"lat": 40.71264206172198648, "lon": -73.95069249193706},
+                             opacity=0.5,
+                             labels={'Count of Trees': 'Count Trees'},
+                             height=700
+                             )
+    m.update_traces(marker_line_width=0)
+
+    return m
+
+
+def boro_counts(species_boro_count):
+    bronx = species_boro_count.loc[species_boro_count['Boro'] == 'Bronx'].sort_values(by='Count', ascending=False)
+    brooklyn = species_boro_count.loc[species_boro_count['Boro'] == 'Brooklyn'].sort_values(by='Count', ascending=False)
+    manhattan = species_boro_count.loc[species_boro_count['Boro'] == 'Manhattan'].sort_values(by='Count',
+                                                                                              ascending=False)
+    queens = species_boro_count.loc[species_boro_count['Boro'] == 'Queens'].sort_values(by='Count', ascending=False)
+    staten_island = species_boro_count.loc[species_boro_count['Boro'] == 'Staten Island'].sort_values(by='Count',
+                                                                                                      ascending=False)
+
+    boro_counts = {'The Bronx': bronx, 'Brooklyn': brooklyn,
+                   'Manhattan': manhattan, 'Queens': queens,
+                   'Staten Island': staten_island}
+    return boro_counts
+
+data = load_trees_data()
+
+# set the trees data to session state
+# If count is already initialized, don't do anything
+if 'data' not in st.session_state:
+    st.session_state.data = data
+
+boroughs_map = boroughs_map()
+nta_map = nta_map()
+hex_map = hex_map()
+
+st.markdown('### Street Tree Overview')
+
+st.markdown("First, let's orient ourselves and take a look at where these 600,000+ street trees are. "
+            "To do this, I've created three maps showing the distribution of street trees across the city. "
+            "The first map breaks down the count of trees by Boro, the second does the same by Neighborhood Tabulation Area, and the third shows trees aggregated up into 1/4 square mile hex-grids. "
+            "Looking at the trees in these three ways, starting very generally, and ending more granularly, we can get a real sense of the city's landscape. ")
+
+st.markdown("Click through the maps and pan around to explore the leafy landscape:")
+
+bl, omap, br = st.columns([1, 3, 1])
+with omap:
+
+    map_option = st.radio(
+        'Select a geography',
+        (['Boro', 'Neighborhood Tabulation Area', '1/4 Sq Mile Hex-Grid']),
+        horizontal=True)
+
+    map_to_make = map_option
+    st.subheader(f'Trees by {map_option}')
+
+    if map_option == 'Boro':
+        st.plotly_chart(boroughs_map, use_container_width=True)
+    elif map_option == 'Neighborhood Tabulation Area':
+        st.plotly_chart(nta_map, use_container_width=True)
+    elif map_option == '1/4 Sq Mile Hex-Grid':
+        st.plotly_chart(hex_map, use_container_width=True)
+
+st.markdown(
+    "##### Click the Species tab to read on and explore the types of trees found on NYC streets!")
+
+st.markdown(
+    "Curious to learn more? You can read more about this great project at this link: https://www.nycgovparks.org/trees/treescount.")
---
+++ pages/1_Species.py
@@ -0,0 +1,113 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+
+st.set_page_config(
+    page_title="Street Tree Species",
+    page_icon=':deciduous_tree:',
+    layout="wide"
+)
+
+@st.cache
+def load_trees_data():
+    usecols = ['created_at', 'tree_id', 'block_id', 'the_geom', 'tree_dbh',
+               'stump_diam', 'curb_loc', 'status', 'health', 'spc_latin', 'spc_common',
+               'address', 'zipcode', 'cb_num', 'boroname', 'nta', 'nta_name',
+               'boro_ct', 'latitude', 'longitude']
+
+    data = pd.read_csv('streamlit/streamlit/2015StreetTreesCensus_TREES.csv', usecols=usecols)
+    data.rename({'latitude':'latitude'}, inplace=True)
+    data['spc_common'] = data['spc_common'].str.title()
+    return data
+
+
+def species_map(data):
+    fig = px.scatter_mapbox(data, lat="Latitude", lon="longitude", hover_data=["address"],
+                            color_discrete_sequence=["green"], zoom=9,
+                            center={"lat": 40.71264206172198648, "lon": -73.95069249193706},
+                            height=700)
+    fig.update_layout(mapbox_style="carto-positron")
+
+    return fig
+
+
+@st.cache
+def load_trees_species_count():
+    species_count = pd.read_csv('streamlit/streamlit/common_species_count.csv')
+    return species_count
+
+
+def top_20_species_chart(species_count):
+    fig = px.bar(species_count.nlargest(n=20, columns=['count']), x='spc_common', y='count', color='count',
+                 labels={"spc_common": "Species",
+                         "count": "Count"},
+                 color_continuous_scale='algae', range_color=[0, 100000])
+    return fig
+
+
+def bottom_20_species_chart(species_count):
+    fig = px.bar(species_count.nsmallest(n=20, columns=['count']), x='spc_common', y='count', color='count',
+                 labels={"spc_common": "Species",
+                         "count": "Count"},
+                 color_continuous_scale='algae', range_color=[0, 100])
+
+    return fig
+
+
+# Load or reload trees data if needed
+data = st.session_state.data
+if 'data' not in st.session_state:
+    st.session_state.data = data
+
+species = load_trees_species_count()
+top20chart = top_20_species_chart(species)
+bottom20chart = bottom_20_species_chart(species)
+
+st.title('A Look at the NYC Tree Census of 2015')
+
+# Notify the reader that the data was successfully loaded.
+# Create a text element and let the reader know the data is loading.
+data_load_state = st.text('Loading data...')
+data_load_state.text('Loading data...done!')
+
+st.header('Street Tree Species')
+
+st.markdown("### Most and Least Common Trees")
+
+st.markdown("Now, let's take a look at all the different species of trees found on New York's streets. "
+            "These two charts show the 20 most and least common trees in the city, while the interactive table to the right lists out the count for every identified species.")
+
+coll, col1, col2, colgap, coltable, colr = st.columns([0.2, 2, 2, 0.25, 1, 0.2])
+
+col1.subheader("20 Most Common Species")
+col1.plotly_chart(top20chart, use_container_width=True)
+
+col2.subheader("20 Least Common Species")
+col2.plotly_chart(bottom20chart, use_container_width=True)
+
+coltable.subheader('All Species')
+coltable.dataframe(species)
+
+st.markdown('### Interactive Species Map')
+
+st.markdown("Finally, let's explore where we can find each species. "
+            "Select a species from the drop down, and then check out the map below to see where we can find it. "
+            "The trees are sorted from most to least common - so be sure to check out those trees at the bottom!")
+
+col3, colmap, col4 = st.columns([1, 3, 1])
+
+with colmap:
+    option = st.selectbox(
+        'Select a species',
+        (list(species['spc_common'].unique())))
+
+    species_to_filter = option
+    filtered_data = data[data['spc_common'] == species_to_filter]
+    st.subheader(f'Map of {species_to_filter}')
+    species_map = species_map(filtered_data)
+    st.plotly_chart(species_map, use_container_width=True)
+
+st.markdown("Thanks for reading! Send any questions or thoughts to mfavorito1995@gmail.com.")
+
+st.markdown(
+    "Curious to learn more? You can read more about this great project at this link: https://www.nycgovparks.org/trees/treescount.")
